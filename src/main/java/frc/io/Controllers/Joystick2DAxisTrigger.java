b5c47d497e73496ca28376a606a938c76eaf00e0
vivtrain <k99vivek@gmail.com>
2024-11-26 00:23:03 -0800

    New Joystick2DAxisTrigger

--- src/main/java/frc/io/Controllers/Joystick2DAxisTrigger.java
+++ src/main/java/frc/io/Controllers/Joystick2DAxisTrigger.java
@@ -8,15 +8,17 @@ import edu.wpi.first.util.ErrorMessages;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-public class JoystickAxisTrigger extends Trigger {
+public class Joystick2DAxisTrigger extends Trigger {
 
-  /** Fires when a GenericHID axis moves outside of a threshold centered around zero
-   * @param genericHID the human interface device to check
-   * @param axis the axis to read, assumed to read zero with no human input
+  /** Fires when a the norm of a pair of axes exceeds the given threshold
+   * @param genericHID the human interface device to check 
+   * @param xAxis the horizontal axis to read, assumed to read zero with no human input
+   * @param yAxis the vertical axis to read, assumed to read zero with no human input
    * @param threshold the +/- threshold outside of which should cause the trigger to fire
    */
-  public JoystickAxisTrigger(GenericHID genericHID, int axis, double threshold) {
-    super(() -> Math.abs(genericHID.getRawAxis(axis)) > threshold);
+  public Joystick2DAxisTrigger(GenericHID genericHID, int xAxis, int yAxis, double threshold) {
+    super(
+      () -> Math.hypot(genericHID.getRawAxis(xAxis), genericHID.getRawAxis(yAxis)) > threshold);
     ErrorMessages.requireNonNullParam(
       genericHID,
       genericHID.getName(),
--- src/main/java/frc/io/Controllers/RegisteredController.java
+++ src/main/java/frc/io/Controllers/RegisteredController.java
@@ -10,28 +10,50 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RegisteredController extends GenericHID {
-    private ControllerRegistrar m_registrar = ControllerRegistrar.getInstance();
+	private ControllerRegistrar m_registrar = ControllerRegistrar.getInstance();
 
-    private HashSet<Integer> m_buttonMappings = new HashSet<Integer>();
-    private HashSet<Integer> m_axisMappings = new HashSet<Integer>();
+	private HashSet<Integer> m_buttonMappings = new HashSet<Integer>();
+	private HashSet<Integer> m_axisMappings = new HashSet<Integer>();
 
-    public RegisteredController(int port) {
-        super(port);
-        m_registrar.registerController(port);
-    }
+	public RegisteredController(int port) {
+		super(port);
+		m_registrar.registerController(port);
+	}
 
-    public JoystickButton registerButtonMap(int buttonID) {
-        if (m_buttonMappings.contains(buttonID))
-            throw new RuntimeException("Multiple mappings on button " + Integer.toString(buttonID) + " of GenericHID " +  Integer.toString(super.getPort()));
-        m_buttonMappings.add(buttonID);
-        return new JoystickButton(this, buttonID);
-    }
+  /** Registers the button as mapped and returns a Trigger to which a Command can be mapped
+   * @param buttonID the button to be registered as used
+   * @return JoystickButton corresponding to the mapping */
+	public JoystickButton registerButtonMap(int buttonID) {
+		if (m_buttonMappings.contains(buttonID))
+			throw new RuntimeException("Multiple mappings on button " + Integer.toString(buttonID) + " of GenericHID " +  Integer.toString(super.getPort()));
+		m_buttonMappings.add(buttonID);
+		return new JoystickButton(this, buttonID);
+	}
 
-    public JoystickAxisTrigger registerTriggerMap(int axisID, double threshold) {
-        if (m_axisMappings.contains(axisID))
-            throw new RuntimeException("Multiple mappings on axis " + Integer.toString(axisID) + " of GenericHID " +  Integer.toString(super.getPort()));
-        m_axisMappings.add(axisID);
-        return new JoystickAxisTrigger(this, axisID, threshold);
-    }
+  /** Registers the axis as mapped and returns a Trigger to which a Command can be mapped
+   * @param axisID the axis to be registered as used
+   * @param threshold the threshold for triggering
+   * @return JoystickAxisTrigger corresponding the mapping */
+	public JoystickAxisTrigger registerAxisMap(int axisID, double threshold) {
+		if (m_axisMappings.contains(axisID))
+			throw new RuntimeException("Multiple mappings on axis " + Integer.toString(axisID) + " of GenericHID " +  Integer.toString(super.getPort()));
+		m_axisMappings.add(axisID);
+		return new JoystickAxisTrigger(this, axisID, threshold);
+	}
+
+  /** Registers a pair of axes as mapped and returns a Trigger to which a Command can be mapped
+   * @param xAxisID the "horizontal" axis to be registered as used
+   * @param yAxisID the "vertical" axis to be registerd as used
+   * @param threshold the threshold for triggering (circular threshold i.e. norm is calculated)
+   * @return Joystick2DAxisTrigger corresponding to the mapping */
+	public Joystick2DAxisTrigger register2DAxisMap(int xAxisID, int yAxisID, double threshold) {
+		if (m_axisMappings.contains(xAxisID))
+			throw new RuntimeException("Multiple mappings on axis " + Integer.toString(xAxisID) + " of GenericHID " +  Integer.toString(super.getPort()));
+		if (m_axisMappings.contains(yAxisID))
+			throw new RuntimeException("Multiple mappings on axis " + Integer.toString(yAxisID) + " of GenericHID " +  Integer.toString(super.getPort()));
+		m_axisMappings.add(xAxisID);
+		m_axisMappings.add(yAxisID);
+		return new Joystick2DAxisTrigger(this, xAxisID, yAxisID, threshold);
+	}
 
 }
